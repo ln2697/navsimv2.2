a4139084d4cb4c355a4767256d33ffaac3cbc36e
Long Nguyen <long.pollehn@gmail.com>
2025-11-07 11:09:58 +0100

    Update

--- scripts/evaluation/run_metric_caching.sh
+++ scripts/evaluation/run_metric_caching.sh
@@ -1,3 +1,14 @@
+#!/usr/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --time=1-00:00:00
+#SBATCH --gres=gpu:0
+#SBATCH --cpus-per-task=16
+#SBATCH --partition=a100-galvani
+#SBATCH --mail-type=FAIL,END
+#SBATCH --mail-user=long.nguyen@student.uni-tuebingen.de
+#SBATCH --mem=200gb
+
 TRAIN_TEST_SPLIT=navtest
 CACHE_PATH=$NAVSIM_EXP_ROOT/metric_cache
 
