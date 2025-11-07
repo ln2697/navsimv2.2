ccedaccdf74eb53e9913dc4210e43875abad2dec
Long Nguyen <long.pollehn@gmail.com>
2025-11-07 20:10:16 +0100

    Update submodule 3rd_party/navsim_workspace/navsimv2.2

--- navsim/agents/carla_transfuser_agent.py
+++ navsim/agents/carla_transfuser_agent.py
@@ -116,8 +116,14 @@ class CarlaTransfuserAgent(AbstractAgent):
             "acceleration": torch.linalg.norm(features["status_feature"][:, 6:8]).reshape(-1, 1),
         })
         
+        future_waypoints = output.pred_future_waypoints.clone()
+        
+        # Model trained in mixed dataset, need to flip y axis
+        future_waypoints[:, :, 1] = -future_waypoints[:, :, 1]
+        future_headings = -output.pred_future_headings.clone()
+        
         return {
-            "trajectory": torch.concatenate([output.pred_future_waypoints, output.pred_future_headings.unsqueeze(-1)], dim=-1).cpu()
+            "trajectory": torch.concatenate([future_waypoints, future_headings.unsqueeze(-1)], dim=-1).cpu()
         }
 
     def compute_loss(
