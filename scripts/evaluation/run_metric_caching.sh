b62e9c0509edbf2410ffa651973c0e8b2f14798e
Long Nguyen <long.pollehn@gmail.com>
2025-11-08 15:49:24 +0100

    Update submodule 3rd_party/navsim_workspace/navsimv2.2

--- scripts/evaluation/run_metric_caching.sh
+++ scripts/evaluation/run_metric_caching.sh
@@ -12,6 +12,9 @@
 TRAIN_TEST_SPLIT=navtest
 CACHE_PATH=$NAVSIM_EXP_ROOT/metric_cache_v2.2
 
+export NAVSIM_DEVKIT_ROOT="${PROJECT_DIR}/3rd_party/navsim_workspace/navsimv2.2"
+export HYDRA_FULL_ERROR=1
+
 python $NAVSIM_DEVKIT_ROOT/navsim/planning/script/run_metric_caching.py \
 train_test_split=$TRAIN_TEST_SPLIT \
 metric_cache_path=$CACHE_PATH
